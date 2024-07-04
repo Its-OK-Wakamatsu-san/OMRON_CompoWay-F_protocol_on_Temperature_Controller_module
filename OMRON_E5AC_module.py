8a9dbe1e8ee53aeed6cd432cc08f2ce5a9547aa7
Its' O.K. Wakamatsu <131073488+Its-OK-Wakamatsu-san@users.noreply.github.com>
2024-07-04 11:09:10 +0900

    Add files via upload

--- OMRON_E5AC_module.py
+++ OMRON_E5AC_module.py
@@ -2,16 +2,16 @@
 # OMRON CompoWay/F Protocol           
 # Temperature Controller E5AC
 import serial
-import time
 from functools import reduce
 from operator import xor
+import time
 
 class Application:
     def __init__(self):
-        # command break down 
+        # command break down   _typical command_    read the model number and buffer size 
         self.cmd_stx_b =  b'\x02'       # start of text
         self.cmd_0     = '01000'        # NODE:01 SubAddress:00 SID:0
-        self.cmd       = '0503'         # MRC/SRC(MainRequestCode/SubRequestCode)  typical comand 
+        self.cmd       = '0503'         # MRC/SRC(MainRequestCode/SubRequestCode)  
         self.cmd_etx_b =  b'\x03'       # end of text 
 
     def Connect_Device(self):
@@ -23,7 +23,7 @@ class Application:
                 stopbits=serial.STOPBITS_TWO,\
                 bytesize=serial.SEVENBITS,\
                 timeout=0 )
-            #print("connected to: " + self.serial_port.portstr)                       #CHG COM15 -> COM3 temporary  CompoWay/F default baudrate is 38400bps 
+            #print("connected to: " + self.serial_port.portstr)                       #CHG COM3 temporary,  CompoWay/F default baudrate is 38400bps 
             setting = self.serial_port.get_settings()
             #print("Serial Setting is : ",setting)
         except:
@@ -36,7 +36,7 @@ class Application:
     
     # write Command & read E5AC buffer
     def Command_Response(self, cmd):
-        cmd_ASCII = self.cmd_0 + cmd + self.cmd_etx_b.decode()          # cmd in ASCII character
+        cmd_ASCII = self.cmd_0 + cmd + self.cmd_etx_b.decode()          # cmd in ASCII characters
 
         fcs = self.Compute_FCS( cmd_ASCII )                             # calculate cmd_ASCII XOR
         fcs_h = int(fcs,16)                                             #INS  change hex_string → integer
@@ -48,36 +48,88 @@ class Application:
         time.sleep(0.05)                                        # Wait a minute (0.1s).    tried minimum wait time  ... about 0.05s
 
         #print("Receive from OMRON E5AC:")
-        buf = self.serial_port.read()                           # read start of text         stx_b =  b'\x02'
-        buf_full = self.serial_port.read_all()                  # b'01000005030000E5AC-TCX4A00D9\x03\x1c'                        
-        #                                                       #   0123456789012345678901234567890
+        buf = self.serial_port.read()                           # read start of text         stx_b =  b'\x02' dummy read
+        buf_full = self.serial_port.read_all()                  # b'01000005030000E5AC-TCX4A00D9\x03\x1c'                                          
         buf_full_ASCII = buf_full.decode()                      #  '01000005030000E5AC-TCX4A00D9  ' 
-        buf_ASCII = buf_full_ASCII[10:]
-
+        buf_ASCII = buf_full_ASCII[10:]                         #   0123456789012345678901234567890
         self.serial_port.close()
 
         return buf_ASCII
     
     def Device_Info(self):  
-        cmd       = '0503'         # MRC/SRC(MainRequestCode/SubRequestCode)
-        buf_ASCII = self.Command_Response(cmd)              #0000E5AC-TCX4A00D9                                         
-        str_ID = buf_ASCII[4:14]                            #0123456789012345678901234
+        cmd       = '0503'         # read the model number and buffer size  MRC/SRC(MainRequestCode/SubRequestCode)
+        buf_ASCII = self.Command_Response(cmd)          #0000E5AC-TCX4A00D9                                         
+        str_ID = buf_ASCII[4:14]                        #0123456789012345678901234
         return str_ID
     
     def Device_Status(self):  
-        cmd       = '0601'         # MRC/SRC(MainRequestCode/SubRequestCode)
-        buf_ASCII = self.Command_Response(cmd)              #00000100                     
-        str_status = buf_ASCII[4:8]                         #0123456789012345678901234
+        cmd       = '0601'         # read the operating status and error status MRC/SRC(MainRequestCode/SubRequestCode)
+        buf_ASCII = self.Command_Response(cmd)          #00000100                     
+        str_status = buf_ASCII[0:]                     #0123456789012345678901234
         return str_status
 
+    def Set_Communication_Write_Enable(self, bool_manual ):        
+        if bool_manual == True:
+            cmd = '30050001'     # Communication Write Enable       # Operation Command MRC/SRC(MainRequestCode/SubRequestCode) & Command and fuction
+        else:
+            cmd = '30050000'     # Communication Write Disable     
+        #print (cmd)
+        str_response = self.Command_Response(cmd)       #0000                                  
+        return str_response
+        
+    def Set_Manual_Mode(self, bool_mode ):        
+        if bool_mode == True:
+            cmd = '30050901'     # Manual Mode                       # Operation Command MRC/SRC(MainRequestCode/SubRequestCode) & Command and fuction
+        else:
+            cmd = '30050900'     # Auto Mode       
+        #print (cmd)
+        str_response = self.Command_Response(cmd)       #0000                                  
+        return str_response
+     
+    def Execute_Process(self, bool_exec ):        
+        if bool_exec == True:
+            cmd = '30050100'     # Run                              # Operation Command MRC/SRC(MainRequestCode/SubRequestCode) & Command and fuction
+        else:
+            cmd = '30050101'     # Stop      
+        #print (cmd)
+        str_response = self.Command_Response(cmd)       #0000                                  
+        return str_response
+    
     def Get_Temp(self):  
-        cmd       = '0101C00000000001'         # MRC/SRC(MainRequestCode/SubRequestCode) & Command
+        cmd       = '0101C00000000001'                      # read data MRC/SRC(MainRequestCode/SubRequestCode) & Command
         #            0123456789012345
-        buf_ASCII = self.Command_Response(cmd)              #000000000018                                  
-        temp_h = buf_ASCII[4:12]                            #0123456789012345678901234
+        buf_ASCII = self.Command_Response(cmd)          #000000000018                                  
+        temp_h = buf_ASCII[4:12]                        #0123456789012345678901234
         temp = int(temp_h,16)
         return temp
-    
+
+    def Set_Output_Value(self, mv ):                                #set Manual Output Value
+        mv_int = int(mv*10)
+        if mv_int < 0:
+            mv_int = 0
+        elif mv_int > 1000:
+            mv_int = 1000
+        mv_int_str   = format(mv_int, '08x')
+        #print (mv_int_str)                                           # .upper() change Small Characters into Large Characters 
+        cmd       = '0102C10024000001'  + mv_int_str.upper()          #  write data MRC/SRC(MainRequestCode/SubRequestCode) & Command 
+        str_response = self.Command_Response(cmd)       #0000                                  
+        return str_response
+     
+    def Get_Output_Value(self):  
+        cmd       = '0101C10024000001'                      # read data MRC/SRC(MainRequestCode/SubRequestCode) & Command
+        #            0123456789012345
+        buf_ASCII = self.Command_Response(cmd)          #000000000064                                  
+        manual_value_h = buf_ASCII[4:12]                #0123456789012345678901234
+        manual_value = int(manual_value_h ,16) /10.0
+        return manual_value
+           
+    def Get_General_Value(self, v_type_str, address_str ,):  
+        cmd       = '0101' + v_type_str + address_str + '000001'        # read data MRC/SRC(MainRequestCode/SubRequestCode) & Command
+        buf_ASCII = self.Command_Response(cmd)          #000000000018
+        status_ASCII = buf_ASCII[:4] 
+        value_ASCII  = buf_ASCII[4:12]                                  
+        return status_ASCII,value_ASCII
+         
 if __name__ == '__main__':
     OMRON_E5AC = Application()
 
@@ -85,20 +137,4 @@ if __name__ == '__main__':
     # import OMRON_E5AC_module
     #         self.OMRON_E5AC   = OMRON_E5AC_module.Application()
     #         str_ID            = OMRON_E5AC.Device_Info() 
-
-    # Check Start Time
-    time_A    = time.time()
-
-    str_ID = OMRON_E5AC.Device_Info()
-    print("Device_Info = ", str_ID)
-
-    str_status = OMRON_E5AC.Device_Status()
-    print("Device_Status =", str_status)
     
-    temp_Present_Value = OMRON_E5AC.Get_Temp()
-    print("Temp_PV (°C) =", temp_Present_Value)
-
-    # Check Start Time
-    time_B     = time.time()
-    turn_around_time = (time_B - time_A)
-    print('Turn_around_time:', turn_around_time)
